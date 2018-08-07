3fb2a0e84067a3e102e0b3914f7ea64bcab59b41
Rubi91 <rubivega91@gmail.com>
2018-08-06 23:41:59 -0500

    Incorporated Login View

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // importar otro módulo
-//import main from './app.js';
+import main from './src/vendors/jquery/jquery.js';
 import './src/vendors/bootstrap/css/bootstrap.min.css';
 import './src/styles/app.scss';
 
-//main();
+main();
